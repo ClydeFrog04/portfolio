a0b5e9575a1c388dbec2c10fc536dd7278e5cba0
ClydeFrog04 <egan.randal@gmail.com>
2023-06-23 08:46:13 -0600

    saving work

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,12 +6,16 @@ import React, {PropsWithChildren} from "react";
 import "./Button.css";
 interface ButtonProps{
     text: string;
+    onClick?: (e: React.MouseEvent) => any;
 }
 
 const Button = (props: PropsWithChildren<ButtonProps>) => {
     const TAG = "[Button.tsx]";
     const handleClick = (e: React.MouseEvent) => {
         console.log(TAG, props.text, "clicked!");
+        if(props.onClick){
+            props.onClick(e);
+        }
     }
 
     return (
