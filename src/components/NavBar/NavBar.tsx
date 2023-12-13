969c7a09bed9057f4e5eff57df99639fb7d43c49
ClydeFrog04 <egan.randal@gmail.com>
2023-12-13 14:25:31 -0700

    drawer added, moving on to rest of mobile styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,12 @@
-import React, {useContext, useEffect, useRef} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 import "./NavBar.scss";
 import beeIcon from "../../../res/TransbeeIconMedium.png";
 import {Link, NavLink, useNavigate} from "react-router-dom";
 import {StateContext} from "../../contexts/StateContext.tsx";
+import {useWindowResize} from "../../hooks/useWindowResize.ts";
+// import Button from "@portfolio-monorepo/calculator/src/components/Button/Button.tsx";
+import Button from "../../components/Button/Button";
+
 
 interface NavBarProps {
 
@@ -12,6 +16,11 @@ const NavBar = (props: NavBarProps) =>{
     const TAG = "[NavBar.tsx]";
     const navigate = useNavigate();
     const {navBarHeight, setNavBarHeight} = useContext(StateContext);
+    const breakpointMobile = 480;// from variables.scss
+    const dimension = useWindowResize();
+    const [devType, setDevType] = useState<"desktop" | "mobile">("desktop");
+    const [menuOpen, setMenuOpen] = useState(false);
+
 
 
     const navBarRef = useRef<HTMLElement|null>(null);
@@ -23,13 +32,19 @@ const NavBar = (props: NavBarProps) =>{
         }
     }, [navBarRef.current]);
 
+    useEffect(() => {
+        console.log(JSON.stringify(dimension));
+        if(dimension.width <= breakpointMobile){
+            console.log("window changed to mobile");
+            setDevType("mobile");
+        } else{
+            console.log("window changed to desktop");
+            setDevType("desktop");
+        }
+    }, [dimension]);
 
-    return (
-        <header className="navBar" ref={navBarRef}>
-            {/*<img className={"beeIcon"} src={beeIcon} onClick={ () => {*/}
-            {/*    navigate("/");*/}
-            {/*}} alt="an icon that is also a bee buzzzzzz"/>*/}
-            <NavLink to={"/"} className={"name"}>Randi Egan</NavLink>
+    const sections =(
+        <>
             <section className="navLinks">
                 <NavLink to={"aboutMe"}>About Me</NavLink>
                 {/*<Link to={"/#projects"}>Projects</Link>*/}
@@ -49,8 +64,44 @@ const NavBar = (props: NavBarProps) =>{
                 {/*    <i className="fa fa-github"/>*/}
                 {/*</a>*/}
             </section>
+        </>
+    )
+
+    const desktop =(
+        <header className="navBar" ref={navBarRef}>
+            {/*<img className={"beeIcon"} src={beeIcon} onClick={ () => {*/}
+            {/*    navigate("/");*/}
+            {/*}} alt="an icon that is also a bee buzzzzzz"/>*/}
+            <NavLink to={"/"} className={"name"}>Randi Egan</NavLink>
+            {sections}
         </header>
-    );
+    )
+
+    const mobile = (
+        <div className={`navBar menuContainer`}>
+            <img src={beeIcon} alt="" onClick={() => {
+                console.log("bee clicked");
+                setMenuOpen(!menuOpen);
+            }}/>
+            <div className={`menu ${menuOpen ? "" : "hide"}`.trimEnd()} onClick={() => {
+                setMenuOpen(false);
+            }}>
+
+                <div className="separator"/>
+                <div className="navSections">
+                    {sections}
+                    <div className="separator"/>
+                    <span>Bored?</span>
+                    <Button class={"boredBtn"} text={"Take me home!"} onClick={() => {
+                        navigate("/");
+                    }}/>
+                </div>
+            </div>
+        </div>
+    )
+
+
+    return devType === "desktop" ? desktop : mobile;
 }
 
 export default NavBar;
